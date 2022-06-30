00a5840d08959144299c4e671def791868db6de3
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-06-30 16:00:28 -0700

    update 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-/*
- *this function print a given string
+/**
+ *  main - main block
+ *  Return: 0
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle \n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
