58b4ca1bedc9badf628541a4711a0dafc6730815
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-06-30 17:17:40 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,10 +7,8 @@
  */
 int main(void)
 {
-	int length = 
-sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	write(2, 
-"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
-length);
+	int l = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
+l);
 	return (1);
 }
