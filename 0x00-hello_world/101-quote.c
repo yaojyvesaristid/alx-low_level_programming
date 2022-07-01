a21891c2e3ec8e849852de40ce5284d078005d1c
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-06-30 17:10:47 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
-#include <unistd.h> 
-#include <string.h> 
+#include <unistd.h>
+#include <string.h>
 /**
  *  main - main block
  *  Return: 1
  */
 int main(void)
 {
-	int string_size = sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", string_size);
+	int length = 
+		sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	write(2, 
+		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 
+		length);
 	return (1);
 }
