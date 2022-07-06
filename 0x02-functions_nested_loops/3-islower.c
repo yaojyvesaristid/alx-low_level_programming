cc66093b1bfdb4cfc218a8751551b50f9cbc0467
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-07-05 18:41:34 -0700

    Edit 3-islower.c file

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *  * _islower - Return 1 if letter is lowercase, 0 if not.
- *   * @c: char type letter
- *    * Return: 1 if lowercase, 0 if not
- *     */
+ * _islower - Return 1 if letter is lowercase, 0 if not.
+ * @lettre: char type letter
+ * Return: 1 if lowercase, 0 if not
+ */
 int _islower(int lettre)
 {
 	if (lettre > 96 && lettre < 123)
