3a82a9696535a16531d76cccfdb99b70165c2b32
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-07-07 10:11:35 -0700

    add solution for 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - main block
+ * Description: Print the sum of even Fibonacci numbers up to a fib value
+ * not exceeding 4,000,000.
+ * Return: 0
+ */
+int main(void)
+{
+	long int i = 1, j = 2, k, somme_paire = 0;
+
+	while (j < 4000000)
+	{
+		if (j % 2 == 0)
+			somme_paire += j;
+		k = j;
+		j += i;
+		i = k;
+	}
+	printf("%ld\n", somme_paire);
+	return (0);
+}
