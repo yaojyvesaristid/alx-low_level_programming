98f4fd71e1db4a4f60375ad374c6920b7e5d2a1d
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-07-07 03:09:31 -0700

    add solution for 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - Add two integers
+ * @num1: int type number
+ * @num2: int type number
+ * Return: result of addition
+ */
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
