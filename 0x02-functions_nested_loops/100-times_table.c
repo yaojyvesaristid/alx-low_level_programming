cd518f8dd65ff016a199c548d1a59d2c0270297b
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-07-07 08:57:11 -0700

    add solution for 100-times_table.c

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+/**
+ * print_times_table - Print the `n` times table, starting with 0.
+ * Description: If `n` is greater than 15 or less than 0, print nothing.
+ * @n: int type number
+ */
+void print_times_table(int n)
+{
+	int i, j, res;
+
+	if (n < 15 || n > 0)
+	{
+		i = 0;
+		while (i <= n)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				res = i * j;
+				if (res > 99)
+				{
+					_putchar(res / 100 + '0');
+					_putchar((res / 10 % 10) + '0');
+					_putchar(res % 10 + '0');
+				}
+				else if (res > 9)
+				{
+					_putchar(' ');
+					_putchar(res / 10 + '0');
+					_putchar(res % 10 + '0');
+				}
+				else if (j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(res + '0');
+				}
+				else
+					_putchar(res + '0');
+				if (j != n)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+			i++;
+		}
+	}
+}
