8351c42de1cf04c86c273860e61e08e0e61f4ac1
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-07-09 04:19:05 -0700

    add solution for 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * more_numbers - print 10 times the numbers from 0 to 14
+ * Description: Can only use _putchar 3 times
+ */
+void more_numbers(void)
+{
+	int i = 0, j = 0;
+
+	while (i < 10)
+	{
+		while (j < 15)
+		{
+			if (j > 9)
+			{
+				_putchar(j / 10 + '0');
+			}
+			_putchar(j % 10 + '0');
+			else
+			j++;
+		}
+		i++;
+	}
+	_putchar('\n');
+}
