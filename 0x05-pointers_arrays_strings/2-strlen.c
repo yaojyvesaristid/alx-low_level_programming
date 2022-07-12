5ba8bcf7db37f7cde3bf7cb0a3b8fafdfb03233e
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-07-11 22:57:34 -0700

    edit betty style

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,16 +1,14 @@
 #include "main.h"
-
 /**
  * _strlen - return the length of the string
  * @s: char type pointer
  * Return: length of string
  */
-
 int _strlen(char *s)
 {
 	int count = 0;
 
 	while (s[count] != '\0')
-		    count++;
+		count++;
 	return (count);
 }
