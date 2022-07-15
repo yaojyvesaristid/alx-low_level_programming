80333ea248e8e1791a4e4352100b5fe625194975
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-07-15 03:42:56 -0700

    edit solution for 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,12 +1,12 @@
 #include "main.h"
 /**
- * _strcat - concatenate two strings
+ * _strncpy - copy a string
  * @dest: char string to copy to
  * @src: char string
  * @n: number of elements to copy in
  * Return: pointer to resulting string `dest`
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
