59172d9a8a77b6fa7c1790bb5729e4bf840eb4c2
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-07-15 04:13:12 -0700

    edit solution for 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,21 +3,18 @@
  * _strncpy - copy a string
  * @dest: char string to copy to
  * @src: char string
- * @n: number of elements to copy in
+ * @n: number of elements to copy
  * Return: pointer to resulting string `dest`
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
-	for (j = 0; src[j] != '\0' && j < n; j++)
-	{
-		*(dest + j) = *(src + j);
-	}
-	while (j < n)
-	{	
-		*(dest + j) = '\0';
-		j++;
-	}
+	for (j = 0; j < n && src[j] != '\0'; j++)
+		dest[j] = src[j];
+
+	for (; j < n; j++)
+		dest[j] = '\0';
+
 	return (dest);
 }
