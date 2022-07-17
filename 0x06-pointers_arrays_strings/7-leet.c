aac07b609a8aa57d1d8bc8c2380bae12961616b6
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-07-17 12:06:32 -0700

    edit solution for 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,6 +1,4 @@
 #include "main.h"
-#include <string.h>
-#include <ctype.h>
 /**
  * leet - encode a string into 1337
  * @s: string
@@ -9,14 +7,15 @@
 char *leet(char *s)
 {
 	char lettres[] = {a, e, o, t, l};
+	char LETTRES[] = {A, E, O, T, L};
 	int values[] = {4, 3, 0, 7, 1};
 	int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; j < strlen(lettres); j++)
+		for (j = 0; j < 5; j++)
 		{
-			if (s[i] == lettre[j] || s[i] == toupper(lettre[j]))
+			if (s[i] == lettres[j] || s[i] == LETTRES[j])
 			{
 				s[i] = values[j];
 			}
