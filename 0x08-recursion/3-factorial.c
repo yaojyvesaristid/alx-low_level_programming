863cd75380f3ed35ae881618f872480bba618131
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-07-20 09:36:17 -0700

    edit solution  for 3-factorial.c

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,7 +6,10 @@
  */
 int factorial(int n)
 {
-	if (n == 1)
+	if (n == 0)
 		return (1);
-	return (n * factorial(n - 1));
+	if (n > 0)
+		return (n * factorial(n - 1));
+	else
+		return (n * factorial(n + 1));
 }
