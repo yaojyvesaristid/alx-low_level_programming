1322f4cb0489e256830ede03d3133f3612e52234
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-07-20 09:10:41 -0700

    add solution for 0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _puts_recursion - Print a string
+ * @s: string
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		_putchar('\n');
+	_putchar(*s);
+	_puts_recursion(++s);
+}
