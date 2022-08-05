31353a7d9b25a07861f54828312a773b00904b1e
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-08-05 16:33:14 -0700

    add solution for dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - structure definition of a dog
+ * @name: character string
+ * @age: float type
+ * @owner: character string
+ * Description: Define a new struct dog with
+ * the following parameters name, age and owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
