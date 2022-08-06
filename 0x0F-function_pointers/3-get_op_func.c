322cbc810398dac6678da71f579e8af14213705b
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-08-06 14:54:42 -0700

    edit solution for 3-

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -16,10 +16,11 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int i;
+	int i = 0, j;
 
-	i = 0;
-	while (i < 5)
+	for (j = 0; s[j] != '\0'; j++)
+		;
+	while (i < 5 && j == 1)
 	{
 		if (operations[i].op[0] == s[0])
 			return (operations[i].f);
