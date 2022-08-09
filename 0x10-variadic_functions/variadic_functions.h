b12c3940accff3d36f3d199baa192c3784eb296d
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-08-09 06:21:02 -0700

    edit solution for 1-

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,4 +1,5 @@
 #ifndef VARIADIC_FUNCTIONS_H
 #define VARIADIC_FUNCTIONS_H
 int sum_them_all(const unsigned int in, ...);
+void print_numbers(const char *separator, const unsigned int in, ...);
 #endif
