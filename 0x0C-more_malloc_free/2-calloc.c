7ad927c348cdd204d891ecabf6799f6e3f85e29d
yaojyvesaristid <yaojyvesaristide@gmail.com>
2022-08-10 04:26:29 -0700

    edit solution for 2-

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,22 +1,25 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _calloc - allocates memory for an array using malloc
- * @nmemb: unsigned int, number of elements
- * @size: unsigned int, size of each element
- * Return: Null if error, else pointer to malloc memory
+ * _calloc - allocates memory for an array given number of elements and size
+ * @nmemb: number of elements
+ * @size: size of each element
+ * Return: Null if error, else pointer to allocated memory
  */
+
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	int *p;
 	unsigned int i;
 
 	if (nmemb <= 0 || size <= 0)
 		return (NULL);
-	ptr = (int *) malloc(nmemb * size);
-	if (ptr == NULL)
+
+	p = malloc(nmemb * size);
+	if (p == NULL)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
-		ptr[i] = 0;
-	return (ptr);
+		p[i] = 0;
+
+	return (p);
 }
