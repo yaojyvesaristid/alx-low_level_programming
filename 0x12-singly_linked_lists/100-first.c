8d51df3f267bec33df64fb1715d2b901ebc98035
yaojyvesaristide <yaojyvesaristide@gmail.com>
2022-08-15 14:57:32 -0700

    add solution for 100

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * before_main - Print statement within this fxn before running main fxn.
+ */
+void myStartMess(void) __attribute__ ((constructor));
+
+void myStartMess(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
