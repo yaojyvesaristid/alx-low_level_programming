d3a90dd110d6dacb446b746a52cf1b62e075f7cb
yaojyvesaristide <yaojyvesaristide@gmail.com>
2022-08-15 15:04:39 -0700

    edit solution for 100-

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,6 +7,6 @@ void myStartMess(void) __attribute__ ((constructor));
 
 void myStartMess(void)
 {
-	printf("You're beat! and yet, you must allow,
-			\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
