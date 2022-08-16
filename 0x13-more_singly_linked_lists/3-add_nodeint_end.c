80e4c9636190f9141f893176a8af41783d2f2ee5
yaojyvesaristide <yaojyvesaristide@gmail.com>
2022-08-16 07:19:04 -0700

    edit solution for 3-

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -3,6 +3,8 @@
 
 /**
  * add_nodeint_end - adds a new node at the end of a 'listint_t' list
+ * @head: pointer to pointer on a head of a list
+ * @n: const value represents the data
  * Return: address of the new element or NULL
  */
 
