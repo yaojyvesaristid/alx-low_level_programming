3eb44379930b3e7a1999225e3245335df99c0e03
yaojyvesaristide <yaojyvesaristide@gmail.com>
2022-08-24 08:53:58 -0700

    add solution for 2-

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - appends text at the end of a file
+ * @filename: name of the file 
+ * @text_content: NULL terminated string to append  
+ * Return: 1 on success or -1 on failure 
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fd, i;
+	size_t writeByte;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, 0_RDWR | 0_APPEND);
+	if (fd == -1)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+
+		writeByte = write(fd, text_content, i);
+		if (writeByte == -1)
+			return (-1);
+	}
+	close(fd);
+
+	return (1);
+}
