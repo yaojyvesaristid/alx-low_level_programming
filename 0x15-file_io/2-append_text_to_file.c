12cad3d4cdbb2b2eb207803d8268a27982c0b29e
yaojyvesaristide <yaojyvesaristide@gmail.com>
2022-08-24 09:02:57 -0700

    edit solution for 2-

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,9 +2,9 @@
 
 /**
  * append_text_to_file - appends text at the end of a file
- * @filename: name of the file 
- * @text_content: NULL terminated string to append  
- * Return: 1 on success or -1 on failure 
+ * @filename: name of the file
+ * @text_content: NULL terminated string to append
+ * Return: 1 on success or -1 on failure
  */
 
 int append_text_to_file(const char *filename, char *text_content)
@@ -15,7 +15,7 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, 0_RDWR | 0_APPEND);
+	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd == -1)
 		return (-1);
 
